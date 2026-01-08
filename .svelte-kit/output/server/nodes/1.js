bb1cba04920d45aa617626adb78dc6434f7b6751
Carl Hartmann <carl.hartmann@dynabase.de>
2026-01-08 17:26:45 +0100

    updates gitignore

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.B-Y1bXHO.js","_app/immutable/chunks/DArtHkHk.js","_app/immutable/chunks/BjbBlfYJ.js","_app/immutable/chunks/E4fo9bj-.js","_app/immutable/chunks/C2HQg65W.js","_app/immutable/chunks/B2Vnf32q.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/5.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 5;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/templates/new/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/5.BTYef1B0.js","_app/immutable/chunks/DArtHkHk.js","_app/immutable/chunks/BjbBlfYJ.js","_app/immutable/chunks/DL2-Z6ex.js","_app/immutable/chunks/LlG9SSuv.js","_app/immutable/chunks/E4fo9bj-.js","_app/immutable/chunks/zHiRsLaD.js","_app/immutable/chunks/C2HQg65W.js","_app/immutable/chunks/B2Vnf32q.js","_app/immutable/chunks/DRZLMWTu.js","_app/immutable/chunks/DAaTc6sE.js","_app/immutable/chunks/BzmJ6XsI.js"];
-export const stylesheets = [];
-export const fonts = [];
